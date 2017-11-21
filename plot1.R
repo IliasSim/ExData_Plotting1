97251f99090d1e999f14e07b69dc82d93bb392f9
IliasSim <iliassimky@gmail.com>
2017-11-21 19:50:08 +0200

    plots for week 1 assiAssignment by iliasS-1

--- plot1.R
+++ plot1.R
@@ -5,8 +5,8 @@
     power_data<-read.table("household_power_consumption.txt",header = TRUE, sep=";")
     feb_power_data<-power_data[power_data$Date=="1/2/2007"|power_data$Date=="2/2/2007",]
     feb_power_data[,3:9]<-apply(feb_power_data[,3:9],2,function(x) as.numeric(as.character(x)))
-    feb_power_data$Date<-as.Date(strptime(as.character(feb_power_data$Date),tz="",format = "%d/%m/%Y"))
-    feb_power_data$Time<-strptime(as.character(h$Time),format = "%H:%M:%S")
+    feb_power_data$Date<-as.Date(strptime(as.character(feb_power_data$Date),tz="GMT",format = "%d/%m/%Y"))
+    feb_power_data$Time<-strptime(as.character(feb_power_data$Time),format = "%H:%M:%S")
 
     #creats plot.1png
     png(file="plot1.png")
